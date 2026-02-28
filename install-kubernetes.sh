b158d05191d59960658c8ba61a880a690844ba73
Vusal Jafarov <vusal.jafarov1999@gmail.com>
2026-02-28 13:19:25 +0100

    Create install-kubernetes.sh

---
+++ install-kubernetes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables=1
+net.bridge.bridge-nf-call-ip6tables=1
+net.ipv4.ip_forward=1
+EOF
+
+sudo sysctl --system
+
+sudo rm -f /etc/apt/sources.list.d/kubernetes.list
+sudo rm -f /usr/share/keyrings/kubernetes-apt-keyring.gpg
+
+sudo mkdir -p /usr/share/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key \
+  | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-apt-keyring.gpg] \
+https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" \
+| sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
